2028eb2dee152111a4eafca003f7a3ac19293b66
Jomain Rouser <jomainrouser12@gmail.com>
2020-04-17 13:57:15 -0500

    added new functions and some front end

---
+++ public/assets/js/ui.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // nav menu
+  const budget = document.querySelectorAll('.budget');
+  
+  // add recipe form
+  const bankExchange = document.querySelectorAll('.bankExchange');
+  const amount = document.querySelectorAll('.amount');
+
+if
+
+});
